1f7af73b61eae22480018e059e2f224dce8a146a
muthukumar-mitrah <muthukumar.r@mitrahsoft.com>
2024-03-07 15:14:35 +0530

    Added the deployed mongo connection string

--- api/database/mongoDb.js
+++ api/database/mongoDb.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 
+const connectionString = "mongodb+srv://rmuthukumar48:JDptKYxhoHIMClOA@instagram-api.r1rbrzx.mongodb.net/?retryWrites=true&w=majority&appName=instagram-api"
+
 mongoose.set('strictQuery', true)  // this is optional
 
-const mongoDB = () => mongoose.connect("mongodb://localhost:27017/instagram")
+
+const mongoDB = () => mongoose.connect(connectionString)
 
 export default mongoDB
